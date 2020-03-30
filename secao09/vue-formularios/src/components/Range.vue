45e8ee6fdfcb84a47364f1001bbf4e7fbf25b5d7
Eduardo Klein <eduardo.klein@catolicasc.org.br>
2020-03-30 16:48:37 -0300

    Adding section 09

---
+++ secao09/vue-formularios/src/components/Range.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <label>{{ customLabel }}</label>
+        <input 
+            type="range"
+            :value="valor"
+            v-bind="$attrs"
+            :class="inputClasses"
+            @change="atualizar">
+    </div>
+</template>
+
+<script>
+export default {
+    inheritAttrs: false,
+    model: {
+      prop: 'valor',
+      event: 'change'
+    },
+    props: {
+        label: String,
+        valor: [Number, String],
+        inputClasses: [String, Object, Array]
+    },
+    computed: {
+        customLabel() {
+            return `${this.label} (R$ ${this.valor || this.$attrs.min})`
+        }
+    },
+    methods: {
+        atualizar() {
+            const valor = event.target.value
+            this.$emit('change', valor)
+        }
+    }
+}
+</script>
